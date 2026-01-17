0e3c6aa274e990e425ab76d85d3c1ccfc9265822
Ibeawuchi chukwugozirim <ibeawuchichukwugozirim@gmail.com>
2026-01-17 14:59:20 +0100

    update

--- learning/admin_urls.py
+++ learning/admin_urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from . import admin_views
 
+app_name = 'learning'
+
 urlpatterns = [
     # Dashboard
     path('', admin_views.custom_admin_dashboard, name='custom_admin_dashboard'),
